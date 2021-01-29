621d84f492702b093d8088219e18b8b4002f7526
Anders <andesob@stud.ntnu.no>
2021-01-29 15:19:24 +0100

    Added class to hold strings

---
+++ lib/Strings/Strings.dart
@@ -0,0 +1,9 @@
+class Strings{
+  static String enabled = "Enabled";
+  static String needs = "Needs";
+  static String custom = "Custom";
+  static String keyboard = "Keyboard";
+  static String contacts = "Contacts";
+  static String smart = "Smart";
+  static String emergency = "Emergency";
+}
