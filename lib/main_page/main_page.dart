744b9269c6035545e653da4097f8582671907468
andesob <andesob@stud.ntnu.no>
2021-02-02 17:47:17 +0100

    Added matching colors and set up darkmode

--- lib/main_page/main_page.dart
+++ lib/main_page/main_page.dart
@@ -23,40 +23,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool sunde = false;
+  bool darkmode = false;
 
   void _changeText() {
     setState(() {
-      sunde = !sunde;
-      print(sunde);
+      darkmode = !darkmode;
+      print(darkmode);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    Color lightPeach = Color(StaticColors.lightPeach);
-    Color darkPeach = Color(StaticColors.darkPeach);
+    Color lightPeach = Color(StaticColors.unbleachedSilk);
+    Color darkPeach = Color(StaticColors.melon);
+    Color appBarColor = Color(StaticColors.apricot);
+    Color backgroundColor = Color(darkmode ? StaticColors.onyx : StaticColors.white);
+
     var isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
 
     return Container(
       decoration: new BoxDecoration(
-          gradient: new LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.topRight,
-              stops: [0.0, 1.0],
-              colors: [lightPeach, darkPeach])),
+        /*gradient: new LinearGradient(
+          colors: [lightPeach, darkPeach],
+          begin: FractionalOffset.topCenter,
+          end: FractionalOffset.bottomCenter,
+        ),*/
+      ),
       child: Scaffold(
+        backgroundColor: backgroundColor,
         appBar: PreferredSize(
           preferredSize: Size.fromHeight(isPortrait ? 50 : 30),
           child: GradientAppBar(
             // Here we take the value from the MyHomePage object that was created by
             // the App.build method, and use it to set our appbar title.
-            gradient: LinearGradient(colors: [lightPeach, darkPeach]),
+            gradient: LinearGradient(colors: [appBarColor, appBarColor]),
             actions: <Widget>[
               Material(
                 type: MaterialType.transparency,
                 child: IconButton(
                     icon: Icon(Icons.accessible_forward),
+                    color: Color(darkmode ? StaticColors.black : StaticColors.white),
                     splashColor: Color(Colors.grey.value),
                     padding: EdgeInsets.zero,
                     onPressed: () {
@@ -70,12 +76,12 @@ class _MyHomePageState extends State<MyHomePage> {
             shrinkWrap: true,
             crossAxisCount: isPortrait ? 2 : 3,
             children: <Widget>[
-              MainPageButton(Strings.needs),
-              MainPageButton(Strings.custom),
-              MainPageButton(Strings.keyboard),
-              MainPageButton(Strings.contacts),
-              MainPageButton(Strings.smart),
-              MainPageButton(Strings.emergency),
+              MainPageButton(Strings.needs, darkmode),
+              MainPageButton(Strings.custom, darkmode),
+              MainPageButton(Strings.keyboard, darkmode),
+              MainPageButton(Strings.contacts, darkmode),
+              MainPageButton(Strings.smart, darkmode),
+              MainPageButton(Strings.emergency, darkmode),
             ]),
       ),
     );
