e7f3ef1aa4767a672aeffe15706b3a83ef1ef585
eskil <eskil.torland@gmail.com>
2021-02-05 10:51:34 +0100

    Added buttons for needs

--- lib/needs/needsPageButton.dart
+++ lib/needs/needsPageButton.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class NeedsPageButton extends StatelessWidget {
   String text;
@@ -17,20 +18,32 @@ class NeedsPageButton extends StatelessWidget {
     const Color lightPeach = Color(0x80ffecd2);
     const Color darkPeach = Color(0x80fcb7a0);
 
-    return Container(
-      margin: EdgeInsets.all(20),
-      decoration: new BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(20)),
-        gradient: new LinearGradient(
-          colors: [lightPeach, darkPeach],
-          begin: Alignment.bottomLeft,
-          end: Alignment.topRight,
-        ),
-      ),
-      child: new BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 10.0,sigmaY: 10.0),
-
-      ),
+    return Row(
+      children: [
+        Flexible(
+            child: new Container(
+              margin: EdgeInsets.all(20),
+              decoration: new BoxDecoration(
+                image: DecorationImage(image: AssetImage('images/flutterimgtest.jpg'),
+                fit: BoxFit.fitHeight),
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                gradient: new LinearGradient(
+                  colors: [lightPeach, darkPeach],
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                ),
+              ),
+              child: new Container(
+                width: 400,
+                height: 400,
+                child: new Text(
+                  text,
+                  style: TextStyle(color: Color(0xffffffff)),
+                ),
+                alignment: Alignment.bottomCenter,
+              ),
+            ))
+      ],
     );
   }
 }
