26372568007b284c071c2078567c344f99e68658
andesob <andesob@stud.ntnu.no>
2021-02-12 13:35:39 +0100

    Found plugin and integrated it to android

--- android/app/src/main/java/no/ntnu/enabled_app/MainActivity.java
+++ android/app/src/main/java/no/ntnu/enabled_app/MainActivity.java
@@ -1,6 +1,18 @@
 package no.ntnu.enabled_app;
 
-import io.flutter.embedding.android.FlutterActivity;
+import androidx.annotation.NonNull;
 
-public class MainActivity extends FlutterActivity {
+import com.google.android.gms.cast.framework.CastContext;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterFragmentActivity {
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        CastContext.getSharedInstance(getApplicationContext());
+    }
 }
