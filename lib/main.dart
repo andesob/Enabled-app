e97859516ccde6fe6e97f35bc1e0a76f34ee1ab8
Rojahno <andrersu@stud.ntnu.no>
2021-03-31 13:37:55 +0200

    Temp socket server in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:enabled_app/contacts_page//contacts.dart';
+import 'package:enabled_app/desktop_connection/server_socket.dart';
 import 'package:enabled_app/home_page/home_page.dart';
 import 'package:enabled_app/keyboard_page/keyboard_page.dart';
 import 'package:enabled_app/libraries/hue/main/bridge.dart';
@@ -24,6 +27,16 @@ void main() {
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
     runApp(new MyApp());
+  });
+  SocketSingleton socket = SocketSingleton();
+  StreamSubscription sub;
+  Stream stream = socket.getStream();
+  sub = stream.listen((value) {
+    /** setState(() {
+        command = value;
+        });
+     **/
+    print("value: " + value);
   });
 }
 
