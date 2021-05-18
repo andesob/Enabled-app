b0fa7f434c1c3396a9dba7189a28901391354a44
Rojahno <andrersu@stud.ntnu.no>
2021-05-18 18:13:47 +0200

    Added documentation to global keys

--- lib/page_global_keys.dart
+++ lib/page_global_keys.dart
@@ -1,6 +1,10 @@
 import 'package:enabled_app/page_state.dart';
 import 'package:flutter/cupertino.dart';
 
+/// The PageGlobalKeys contains the [GlobalKey] for each page. [GlobalKey] is a
+/// key that is unique across the entire app. [GlobalKey] can be used to access
+/// information about another widget in a different part of the widget tree and
+/// allows for change of a parent widget without loosing the state.
 class PageGlobalKeys {
   static GlobalKey<PageState> homePageKey = GlobalKey();
   static GlobalKey<PageState> needsPageKey = GlobalKey();
