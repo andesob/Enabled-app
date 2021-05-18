e59d630e9480ef51437e11ee95e17685928e917b
Rojahno <andrersu@stud.ntnu.no>
2021-05-18 17:14:53 +0200

    Added documentation for desktop connection

--- lib/desktop_connection/network_service.dart
+++ lib/desktop_connection/network_service.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:wifi/wifi.dart';
 import 'dart:developer' as developer;
 
+/// Returns the local IP address of the device. Does not work on cellular networks.
 class NetworkService {
   static Future<String> get localIP async {
     try {
